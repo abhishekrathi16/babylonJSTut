7a3b853648f1bf1e8ea61415e62c9e8528209058
abhishekrathi16 <rathiabhishek53@gmail.com>
2022-08-08 22:39:45 +0530

    initialise the project

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import useStore from "../store/store"
+
+const Home: NextPage = () => {
+  const color = useStore((state)=>state.color)
+  const changeColor = useStore((state)=>state.changeColor)
+  return (
+    <div className={styles.container}>
+      <div onClick={changeColor} className="iconBox">..reset code..</div>
+      <h1>{color}</h1>
+    </div>
+  )
+}
+
+export default Home
