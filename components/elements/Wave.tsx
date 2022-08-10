8da1642ed4c786a478710ae6dae819b58b5adde9
abhishekrathi16 <rathiabhishek53@gmail.com>
2022-08-10 23:56:47 +0530

    set up ts utility

---
+++ components/elements/Wave.tsx
@@ -0,0 +1,5 @@
+import { Animation, Vector3 } from "@babylonjs/core";
+import React,{ useEffect, useState } from 'react'
+import {Engine, Scene, useScene} from "react-babylonjs"
+import * as R from 'remeda'
+import tw from 'twin.macro'
